5d518bbbf4b22bb3e2be4e60e3d5e619cf339f39
kirandeep17 <gagankumar2050@gmail.com>
2021-05-30 07:08:23 +0530

    my first commit

---
+++ src/main/java/com/mybeans/demo/BeanScopeDemo2Application.java
@@ -0,0 +1,13 @@
+package com.mybeans.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanScopeDemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanScopeDemo2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mybeans/demo/BeanScopeDemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mybeans.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanScopeDemo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
